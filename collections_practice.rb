2648eeb1eb80b474b917d2f8fd5f3f728fd58deb
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-06-25 19:00:45 -0500

    done

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+require 'pry'
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort_by(&:length)
+end
+
+def swap_elements(array)
+    swap = [array[0], array[2], array[1]]
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each {|x| x[2] = "$" }
+end
+
+def find_a(array)
+    array.select { |x| x[0] == "a" }
+end
+
+def sum_array(array)
+    array.inject(:+)
+end
+
+def add_s(array)
+    array.each_with_index.collect do |element, index|
+        if index != 1
+            element << "s"
+        else
+            element
+        end
+    end
+end
